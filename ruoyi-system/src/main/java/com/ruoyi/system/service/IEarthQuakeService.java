f6a80ab5f9550a0abc07bb150dad379613159e82
Qiuhuawa <1010565096@qq.com>
2025-09-01 16:32:34 +0800

    报告生成

--- ruoyi-system/src/main/java/com/ruoyi/system/service/IEarthQuakeService.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/IEarthQuakeService.java
@@ -1,13 +1,14 @@
 package com.ruoyi.system.service;
 
 import com.ruoyi.common.core.domain.R;
+import com.ruoyi.system.domain.dto.TriggerDTO;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public interface IEarthQuakeService {
-    public R<String> generateEarthQuakeReport(Integer eq_id) throws IOException, InvalidFormatException;
+    public R<String> generateEarthQuakeReport(TriggerDTO triggerDTO) throws IOException, InvalidFormatException;
 
     public void downloadReport(String fileName, HttpServletResponse resp) throws IOException;
 }
--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/EarthQuakeReportServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/EarthQuakeReportServiceImpl.java
@@ -2,6 +2,7 @@ package com.ruoyi.system.service.impl;
 
 import com.ruoyi.common.core.domain.R;
 import com.ruoyi.system.domain.EarthQuakeReportEntity;
+import com.ruoyi.system.domain.dto.TriggerDTO;
 import com.ruoyi.system.service.IEarthQuakeService;
 import org.apache.poi.xwpf.usermodel.*;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTBorder;
@@ -30,7 +31,7 @@ public class EarthQuakeReportServiceImpl implements IEarthQuakeService {
 
     //生成报告
     @Override
-    public R<String> generateEarthQuakeReport(Integer disasterId) throws IOException {
+    public R<String> generateEarthQuakeReport(TriggerDTO triggerDTO) throws IOException {
         // 获取报告数据
         EarthQuakeReportEntity earthQuakeReportEntity = new EarthQuakeReportEntity();
 
@@ -45,7 +46,7 @@ public class EarthQuakeReportServiceImpl implements IEarthQuakeService {
         // 表头宽度
         // int[] widths = {1500, 3000, 8000, 3000};
         try {
-            new CreateRainReport().createEarthQuakeReport(wordPath, earthQuakeReportEntity);
+            new CreateEarthQuakeReport().createEarthQuakeReport(wordPath, earthQuakeReportEntity);
         }catch (IOException e) {
             e.printStackTrace();
         }
@@ -67,9 +68,9 @@ public class EarthQuakeReportServiceImpl implements IEarthQuakeService {
             double flg = Math.random() * 50000;
 
             earthQuakeReportEntity.setEarthQuakeCountry("长安区");
-            earthQuakeReportEntity.setEarthQuakePopulationDensity(flg + "/km²");
+            earthQuakeReportEntity.setEarthQuakePopulationDensity(flg + "");
             earthQuakeReportEntity.setEarthQuakeIntensity("9");
-            earthQuakeReportEntity.setEarthQuakeDisasterArea("50km²");
+            earthQuakeReportEntity.setEarthQuakeDisasterArea("50");
             earthQuakeReportEntity.setEarthQuakeSumGDP("4");
             earthQuakeReportEntity.setEarthQuakeInfluencePopulation(flg * 0.8 + "");
             earthQuakeReportEntity.setEarthQuakeDeath(flg * 0.4 + "");
@@ -87,7 +88,7 @@ public class EarthQuakeReportServiceImpl implements IEarthQuakeService {
                 earthQuakeReportEntity.getEarthQuakeHospital().add(hospital);
             }
 
-            new CreateRainReport().createEarthQuakeReport("D:/test.docx", earthQuakeReportEntity);
+            new CreateEarthQuakeReport().createEarthQuakeReport("D:/test.docx", earthQuakeReportEntity);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -129,7 +130,7 @@ public class EarthQuakeReportServiceImpl implements IEarthQuakeService {
 /**
  * word文档生成
  */
-class CreateRainReport {
+class CreateEarthQuakeReport {
     private static final Integer TEXT_FONT_SIZE = 16;
     private static final String TEXT_FONT_FAMILY = "黑体";
     // 两个字符
@@ -285,20 +286,14 @@ class CreateRainReport {
         XWPFParagraph contentParagraph = createParagraphWithoutNumbering(doc);
         XWPFRun contentRun = contentParagraph.createRun();
 
-        String content = String.format("据地震台网测定，%s（北京时间）在xxxx市xxxx县xxxx镇（北纬xx度，东经xx度）发生xx级地震, 震源深度xx千米。",
-                earthQuakeReportEntity.getRainTime(),
-                list2Str(earthQuakeReportEntity.getRainAreaPosition(), null),
-                list2Str(earthQuakeReportEntity.getRainAreaQuantity(), "毫米"),
-                earthQuakeReportEntity.getConcentratedAreaPosition(),
-                earthQuakeReportEntity.getConcentratedAreaQuantity(),
-                earthQuakeReportEntity.getConcentratedAreaAverageQuantity(),
-                list2Str(earthQuakeReportEntity.getConcentratedAreaDetailStreet(), null),
-                earthQuakeReportEntity.getConcentratedAreaDetailQuantity(),
-                earthQuakeReportEntity.getConcentratedAreaDetailGrade(),
-                streetPlusRainfall(earthQuakeReportEntity.getExtremelyHeavyRainstormStreet(), earthQuakeReportEntity.getExtremelyHeavyRainQuantity()),
-                streetPlusRainfall(earthQuakeReportEntity.getRainstormStreet(), earthQuakeReportEntity.getRainstormQuantity())
+        String content = String.format("据地震台网测定，%s（北京时间）在%s（北纬%s，东经%s）发生%s级地震, 震源深度%s千米。",
+                earthQuakeReportEntity.getEarthQuakeTime(),
+                earthQuakeReportEntity.getEarthQuakePosition(),
+                earthQuakeReportEntity.getEarthQuakeLon(),
+                earthQuakeReportEntity.getEarthQuakeLat(),
+                earthQuakeReportEntity.getEarthQuakeMagnitude(),
+                earthQuakeReportEntity.getEarthQuakeSourceDepth()
         );
-
         contentRun.setText(content);
         contentRun.setFontFamily(TEXT_FONT_FAMILY);
         contentRun.setFontSize(TEXT_FONT_SIZE);
@@ -323,98 +318,45 @@ class CreateRainReport {
         XWPFParagraph contentParagraph = createParagraphWithoutNumbering(doc);
         XWPFRun contentRun = contentParagraph.createRun();
 
-        String content = String.format("受持续强降雨影响，灾害风险评估模型在%d个地质灾害风险区、%d个地质灾害在测隐患点的范围内，结合了%s这些致灾因子进行评估，" +
-                        "评估得到%s%s的地质灾害风险显著上升，需高度警惕其中的%d个地质风险区和%d个地质灾害在测隐患点发生山洪、泥石流等次生灾害发生的可能性。",
-                earthQuakeReportEntity.getRiskAreaQuantity(),
-                earthQuakeReportEntity.getHideAreaQuantity(),
-                list2Str(earthQuakeReportEntity.getHazards(), null),
-                earthQuakeReportEntity.getSignificantIncreaseArea(),
-                list2Str(earthQuakeReportEntity.getSignificantIncreaseAreaStreet(), null),
-                earthQuakeReportEntity.getSignificantIncreaseAreaRiskQuantity(),
-                earthQuakeReportEntity.getSignificantIncreaseAreaHideQuantity()
+        String content = String.format("本次地震震中所在地区%s，人口密度为%s每平方公里。" +
+                        "本次地震重灾区烈度预计达到%s度，重灾区面积为%s平方公里，灾区总GDP为%s亿元；地震影响人口约%s万人，预计伤亡人数%s人。",
+                earthQuakeReportEntity.getEarthQuakeCountry(),
+                earthQuakeReportEntity.getEarthQuakePopulationDensity(),
+                earthQuakeReportEntity.getEarthQuakeIntensity(),
+                earthQuakeReportEntity.getEarthQuakeDisasterArea(),
+                earthQuakeReportEntity.getEarthQuakeSumGDP(),
+                earthQuakeReportEntity.getEarthQuakeInfluencePopulation(),
+                earthQuakeReportEntity.getEarthQuakeDeath()
         );
 
         contentRun.setText(content);
         contentRun.setFontFamily(TEXT_FONT_FAMILY);
         contentRun.setFontSize(TEXT_FONT_SIZE);
 
-        // 滑坡、泥石流、山洪、内涝分别处理
-        for (int i = 0; i < earthQuakeReportEntity.getSecondaryDisasterReport().size(); i++) {
-            RainReportEntity.SecondaryDisasterReportEntity disasterReport = earthQuakeReportEntity.getSecondaryDisasterReport().get(i);
+        // 设置表格标题
+        XWPFParagraph tableTitleParagraph = doc.createParagraph();
+        tableTitleParagraph.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun tableTitleRun = tableTitleParagraph.createRun();
 
-            XWPFParagraph contentParagraph2 = createParagraphWithoutNumbering(doc);
-            XWPFRun contentRun2 = contentParagraph2.createRun();
+        tableTitleRun.setText("50公里内医院统计表");
+        tableTitleRun.setFontFamily(FONT_FANG_SONG);
+        tableTitleRun.setFontSize(14);
 
-            StringBuilder contentBuilder = new StringBuilder();
-            contentRun2.setFontFamily(TEXT_FONT_FAMILY);
-            contentRun2.setFontSize(TEXT_FONT_SIZE);
-
-            if (i == 0) {
-                contentBuilder.append("测算结果根据降雨集中区域结合地形地势因素分析得出，");
-            }
-
-            // 灾害类型
-            contentBuilder.append(
-                    String.format("%s风险主要集中在%s附近区域，其中%s点位滑坡发生概率达%s。" +
-                                    "预计受影响的人数为%d人，灾害等级将达特大型，为本轮强降雨期间%s风险最高区域，需要提前疏散居民，严加防范。" +
-                                    "除此之外，在持续性降雨背景下，%s可能发生的灾害等级将达到大型、中型。",
-                            disasterReport.getDisasterType().getDisasterName(),
-                            disasterReport.getRiskStreet(),
-                            disasterReport.getRiskPointName(),
-                            disasterReport.getRiskPointProbability(),
-                            disasterReport.getInfluencePeopleQuantity(),
-                            disasterReport.getDisasterType().getDisasterName(),
-                            list2Str(disasterReport.getSeriousArea(), null)
-                    )
-            );
-
-            contentRun2.setText(contentBuilder.toString());
-
-            // 设置表格标题
-            XWPFParagraph tableTitleParagraph = doc.createParagraph();
-            tableTitleParagraph.setAlignment(ParagraphAlignment.CENTER);
-            XWPFRun tableTitleRun = tableTitleParagraph.createRun();
-
-            tableTitleRun.setText(String.format("%s灾害预测概率统计表", disasterReport.getDisasterType().getDisasterName()));
-            tableTitleRun.setFontFamily(FONT_FANG_SONG);
-            tableTitleRun.setFontSize(14);
-
-            // 生成表格
-            createSecondaryDisastersTable(doc, disasterReport,
-                    new String[]{"序号", "位置", disasterReport.getDisasterType().getDisasterName() + "发生概率", "灾害等级"});
-
-            // 最后一部分
-            XWPFParagraph contentParagraph3 = doc.createParagraph();
-            XWPFRun contentRun3 = contentParagraph3.createRun();
-
-            contentRun3.setFontFamily(TEXT_FONT_FAMILY);
-            contentRun3.setFontSize(TEXT_FONT_SIZE);
-            contentRun3.setText(
-                    String.format(
-                            "其中，%s%s可能的特大型/大型灾害附近涉及%d个风险区（村庄），预计影响%d人，附近居民和风险影响区域居民必须撤离。" +
-                                    "%s%s可能的中/小型灾害，预计影响%d人，建议附近居民做好防护，风险影响区域居民建议撤离。",
-                            disasterReport.getExtraLargeArea(),
-                            disasterReport.getExtraLargeAreaPoint(),
-                            disasterReport.getExtraLargeAreaRiskQuantity(),
-                            disasterReport.getExtraLargeAreaRiskPeopleQuantity(),
-                            disasterReport.getSmallArea(),
-                            disasterReport.getSmallAreaPoint(),
-                            disasterReport.getSmallAreaRiskPeopleQuantity()
-                    )
-            );
-        }
+        // 生成表格
+        createTable(doc, earthQuakeReportEntity,
+                new String[]{ "序号", "医院名称", "地址", "医院等级", "总床位"});
     }
 
     /**
      * 设置表格数据
      *
      * @param doc
-     * @param secondaryDisasterReportEntity
+     * @param earthQuakeReportEntity
      * @param headers
      */
-    private void createSecondaryDisastersTable(XWPFDocument doc,
-                                               RainReportEntity.SecondaryDisasterReportEntity secondaryDisasterReportEntity, String[] headers) {
-        int rows = secondaryDisasterReportEntity.getDisasterTableData().size() + 1;
+    private void createTable(XWPFDocument doc,
+                                               EarthQuakeReportEntity earthQuakeReportEntity, String[] headers) {
+        int rows = earthQuakeReportEntity.getEarthQuakeHospital().size();
         int cols = headers.length;
         XWPFTable table = doc.createTable(rows, cols);
 
@@ -438,17 +380,19 @@ class CreateRainReport {
             XWPFTableRow bodyRow = table.getRow(i);
             bodyRow.setHeight(TABLE_ROW_HEIGHT);
 
-            RainReportEntity.SecondaryDisasterReportEntity.SecondaryDisasterTableData rowData =
-                    secondaryDisasterReportEntity.getDisasterTableData().get(i - 1);
+            EarthQuakeReportEntity.Hospital hospitalData =
+                    earthQuakeReportEntity.getEarthQuakeHospital().get(i - 1);
 
             // 序号列
             setupTableCell(bodyRow.getCell(0), String.valueOf(i), false);
+            // 医院名称列
+            setupTableCell(bodyRow.getCell(1), hospitalData.getHospitalName(), true);
             // 位置列
-            setupTableCell(bodyRow.getCell(1), rowData.getPosition(), false);
-            // 概率列
-            setupTableCell(bodyRow.getCell(2), rowData.getProbability(), false);
+            setupTableCell(bodyRow.getCell(2), hospitalData.getHospitalAddress(), false);
             // 等级列
-            setupTableCell(bodyRow.getCell(3), rowData.getGrade(), false);
+            setupTableCell(bodyRow.getCell(3), hospitalData.getHospitalLevel(), false);
+            // 床位列
+            setupTableCell(bodyRow.getCell(4), hospitalData.getHospitalBeds(), false);
         }
     }
 
@@ -501,17 +445,18 @@ class CreateRainReport {
 
         // 段落内容数组
         String[] contents = {
-                "防范总建议：",
-                String.format("1. %s政府要做好地质灾害防范工作安排部署，组织镇街、村组开展地质灾害隐患点、风险区巡查排查监测，必要时组织受威胁群众转移避险。",
-                        list2Str(earthQuakeReportEntity.getWorkScheduleArea(), null)),
-                "2. 资源规划部门要加强与相关部门沟通衔接，及时叫应预警区内地质灾害隐患点、风险区监测员和巡查员，督促指导有关单位做好重点区域巡查排查技术指导。",
-                "3.水务、交通、旅游等部门开展预警区内水库、公路、景区等重要基础设施周边地质灾害风险隐患巡查监测。",
-                "4.应急管理部门做好可能发生的地质灾害应急准备工作。",
+                String.format("本次地震初步判定本次地震事件属于较大地震灾害，建议启动%s级地震应急响应。",
+                        earthQuakeReportEntity.getEarthQuakeEmergencyLevel()),
+                String.format("目前灾区灾情信息未知，据初步估计本次地震极震区为%1$s度。" +
+                                "建议由市抗震救灾指挥部提出%1$s级应急响应建议，经市委、市政府同意，由市政府宣布启动地震%1$s级应急响应。"
+                        + "在国务院、省抗震救灾指挥部的统一领导、指挥和协调下，市抗震救灾指挥部负责组织实施全市抗震救灾工作。",
+                        earthQuakeReportEntity.getEarthQuakeEmergencyLevel()),
+                "先期处置：",
+                "（1）市防震减灾服务中心迅速对地震影响进行评估，通报有关领导和部门,并立即向市应急管理局报告震情、预估烈度、灾情初步判断意见，提出抗震救灾工作建议。",
+                "（2）市经济和信息化局、市教育局、市公安局、市自然资源和规划局、市生态环境局、市住房和城乡建设局、市交通运输局、市水利局、市卫生健康委、市通信发展办、国网雅电集团等按照职责及时收集灾情信息通报市应急管理局。",
+                "（3）市应急管理局迅速汇总灾情，组织开展灾情评估，并按规定向市委、市政府、市应急委、应急厅报告，提出应急响应建议。集结抢险救援队伍，调集本地救灾物资，组织支援力量进入备战状态。",
+                "（4）市抗震救灾指挥部成员单位迅速按照职能分工和应急预案规定进行先期处置，及时向市委、市政府、市应急委、市抗震救灾指挥部报告工作进展，相关单位负责同志立即前往市应急管理局参加紧急会议。",
                 "处置措施建议：",
-                String.format("人员疏散方面，建议优先组织%s中紧邻河道的民房、农家乐等高风险区域居民转移，此类区域靠近水体，受山洪和泥石流突发影响最为显著。" +
-                                "同时，应重点关注%s等城市内涝易发的低洼积水区域，确保上述重点区域人员能够及时、安全撤离，最大限度保障群众生命安全。",
-                        list2Str(earthQuakeReportEntity.getEvacuateTheCrowdArea(), null),
-                        list2Str(earthQuakeReportEntity.getFocusArea(), null)),
                 "人员安置方面，应优先选择地势较高、远离河道且具备较好排水条件的安全区域，村内学校、村委会等公共设施也可作为临时安置点，具备一定容纳能力和生活配套条件。对于本地安置条件受限的村组，可组织跨区域转移，安排至周边安全城镇，利用当地酒店、学校等资源保障群众基本生活与应急避险需求。",
                 "救援队伍准备方面：建议提前联系消防、交通和医疗三类专业救援力量：",
                 "消防救援队准备生命探测设备、环境探测设备和破拆工具等，可以及时定位被困人员，评估现场环境。",
