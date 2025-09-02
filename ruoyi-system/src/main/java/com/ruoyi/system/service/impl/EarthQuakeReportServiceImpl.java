79411b7c10cfb5827cbc35424718df633257d01c
Qiuhuawa <1010565096@qq.com>
2025-09-02 09:26:42 +0800

    报告生成-模版模块

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/EarthQuakeReportServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/EarthQuakeReportServiceImpl.java
@@ -240,7 +240,7 @@ class CreateEarthQuakeReport {
         XWPFParagraph paragraph1 = DocumentUtils.addRegularParagraph(doc, null);
 
         String content1 = String.format("本次地震震中所在地区%s，人口密度为%s每平方公里。" +
-                        "本次地震重灾区烈度预计达到%s度，重灾区面积为%s平方公里，灾区总GDP为%s亿元；地震影响人口约%s万人，预计伤亡人数%s人。",
+                        "本次地震重灾区烈度预计达到%s度，重灾区面积为%s平方米，灾区总GDP为%s亿元；地震影响人口约%s人，预计伤亡人数%s人。",
                 earthQuakeReportEntity.getEarthQuakeCountry(),
                 earthQuakeReportEntity.getEarthQuakePopulationDensity(),
                 earthQuakeReportEntity.getEarthQuakeIntensity(),
@@ -252,9 +252,9 @@ class CreateEarthQuakeReport {
 
         DocumentUtils.addRegularRun(paragraph1, content1);
         DocumentUtils.insertImageWithCaption(doc,
-                earthQuakeReportEntity.getEarthQuakeInfluenceGraph(),
-//                "http://t1arte4v9.hb-bkt.clouddn.com/T2024060117164151180001_%E9%9C%87%E5%8C%BA%E9%99%84%E8%BF%91%E5%8C%BB%E7%96%97%E6%9C%BA%E6%9E%84%E5%88%86%E5%B8%83%E5%9B%BE?e=1755938687&token=mheaTe3xRCkChSjwfueGYzB32yi7yk2sj8pemjvF:i6Ni-UdI8wmPLErW4fK8aLYLbEo=",
-                ImageTypeEnum.JPG ,null, null, "图1", ImagePositionEnum.AFTER);
+//                earthQuakeReportEntity.getEarthQuakeInfluenceGraph(),
+                "http://t1arte4v9.hb-bkt.clouddn.com/T2024060117164151180001_%E9%9C%87%E5%8C%BA%E9%99%84%E8%BF%91%E5%8C%BB%E7%96%97%E6%9C%BA%E6%9E%84%E5%88%86%E5%B8%83%E5%9B%BE?e=1755938687&token=mheaTe3xRCkChSjwfueGYzB32yi7yk2sj8pemjvF:i6Ni-UdI8wmPLErW4fK8aLYLbEo=",
+                ImageTypeEnum.JPG ,null, null, "图1 地震影响范围分布图", ImagePositionEnum.AFTER);
 
         XWPFParagraph xwpfParagraph = DocumentUtils.addRegularParagraph(doc, null);
         XWPFRun run = DocumentUtils.addRegularRun(xwpfParagraph, "1.震中附近活动断裂分布");
@@ -267,9 +267,9 @@ class CreateEarthQuakeReport {
         xwpfParagraph.setIndentationFirstLine(0);
 
         DocumentUtils.insertImageWithCaption(doc,
-                earthQuakeReportEntity.getEarthQuakeFaultZoneGraph(),
-//                "http://t1arte4v9.hb-bkt.clouddn.com/T2024060117164151180001_%E9%9C%87%E5%8C%BA%E9%99%84%E8%BF%91%E5%8C%BB%E7%96%97%E6%9C%BA%E6%9E%84%E5%88%86%E5%B8%83%E5%9B%BE?e=1755938687&token=mheaTe3xRCkChSjwfueGYzB32yi7yk2sj8pemjvF:i6Ni-UdI8wmPLErW4fK8aLYLbEo=",
-                ImageTypeEnum.JPG, null, null, "图2", ImagePositionEnum.AFTER);
+//                earthQuakeReportEntity.getEarthQuakeFaultZoneGraph(),
+                "http://t1arte4v9.hb-bkt.clouddn.com/T2024060117164151180001_%E9%9C%87%E5%8C%BA%E9%99%84%E8%BF%91%E5%8C%BB%E7%96%97%E6%9C%BA%E6%9E%84%E5%88%86%E5%B8%83%E5%9B%BE?e=1755938687&token=mheaTe3xRCkChSjwfueGYzB32yi7yk2sj8pemjvF:i6Ni-UdI8wmPLErW4fK8aLYLbEo=",
+                ImageTypeEnum.JPG, null, null, "图2 震中附近断裂带分布图", ImagePositionEnum.AFTER);
 
         XWPFParagraph xwpfParagraph1 = DocumentUtils.addRegularParagraph(doc, null);
         XWPFRun run1 = DocumentUtils.addRegularRun(xwpfParagraph1, "2.震中附近医院分布");
@@ -277,9 +277,9 @@ class CreateEarthQuakeReport {
         run1.setBold(true);
 
         DocumentUtils.insertImageWithCaption(doc,
-                earthQuakeReportEntity.getEarthQuakeHospitalGraph(),
-//                "http://t1arte4v9.hb-bkt.clouddn.com/T2024060117164151180001_%E9%9C%87%E5%8C%BA%E9%99%84%E8%BF%91%E5%8C%BB%E7%96%97%E6%9C%BA%E6%9E%84%E5%88%86%E5%B8%83%E5%9B%BE?e=1755938687&token=mheaTe3xRCkChSjwfueGYzB32yi7yk2sj8pemjvF:i6Ni-UdI8wmPLErW4fK8aLYLbEo=",
-                ImageTypeEnum.JPG, null, null, "图3", ImagePositionEnum.AFTER);
+//                earthQuakeReportEntity.getEarthQuakeHospitalGraph(),
+                "http://t1arte4v9.hb-bkt.clouddn.com/T2024060117164151180001_%E9%9C%87%E5%8C%BA%E9%99%84%E8%BF%91%E5%8C%BB%E7%96%97%E6%9C%BA%E6%9E%84%E5%88%86%E5%B8%83%E5%9B%BE?e=1755938687&token=mheaTe3xRCkChSjwfueGYzB32yi7yk2sj8pemjvF:i6Ni-UdI8wmPLErW4fK8aLYLbEo=",
+                ImageTypeEnum.JPG, null, null, "图3 震中附近医院分布图", ImagePositionEnum.AFTER);
 
 
         // 设置表格标题
@@ -287,7 +287,7 @@ class CreateEarthQuakeReport {
         tableTitleParagraph.setAlignment(ParagraphAlignment.CENTER);
         XWPFRun tableTitleRun = tableTitleParagraph.createRun();
 
-        tableTitleRun.setText("50公里内医院统计表");
+        tableTitleRun.setText("20公里内医院统计表");
         tableTitleRun.setFontFamily(FONT_FANG_SONG);
         tableTitleRun.setFontSize(DocumentConfig.FONT_SIZE_SMALL_FOUR);
 
