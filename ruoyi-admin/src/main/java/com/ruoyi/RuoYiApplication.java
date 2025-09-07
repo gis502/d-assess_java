d6f8cdd12a87aadea0fc19cc7e9e999cdd2429a8
xiaodemos <11165536+xiaodemos@user.noreply.gitee.com>
2025-09-08 00:56:55 +0800

    部署版

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -15,20 +15,28 @@ import java.io.FileNotFoundException;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RuoYiApplication {
     public static void main(String[] args) {
-        // System.setProperty("spring.devtools.restart.enabled", "false");
-        SpringApplication.run(RuoYiApplication.class, args);
 
-        System.out.println("" +
-                "(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              "
-        );
+        try{
+
+            com.supermap.data.LibraryBinPath.setBinPath("/home/env/supermap/Bin");
+            SpringApplication.run(RuoYiApplication.class, args);
+            System.out.println("" +
+                    "(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                    " .-------.       ____     __        \n" +
+                    " |  _ _   \\      \\   \\   /  /    \n" +
+                    " | ( ' )  |       \\  _. /  '       \n" +
+                    " |(_ o _) /        _( )_ .'         \n" +
+                    " | (_,_).' __  ___(_ o _)'          \n" +
+                    " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                    " |  | \\ `'   /|   `-'  /           \n" +
+                    " |  |  \\    /  \\      /           \n" +
+                    " ''-'   `'-'    `-..-'              "
+            );
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+
     }
 }
