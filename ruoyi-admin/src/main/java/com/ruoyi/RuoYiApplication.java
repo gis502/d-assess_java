948706e142ece0cde3573d730f7998962bc94bd6
xiaodemos <1720286998@qq.com>
2025-09-11 18:59:30 +0800

    本地版本（请勿拉取合并！）

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -17,7 +17,6 @@ public class RuoYiApplication {
     public static void main(String[] args) {
 
         try{
-            com.supermap.data.LibraryBinPath.setBinPath("/home/env/supermap/Bin");
             SpringApplication.run(RuoYiApplication.class, args);
             System.out.println("" +
                     "(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
--- ruoyi-common/src/main/java/com/ruoyi/common/constant/BaseConstants.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/constant/BaseConstants.java
@@ -39,8 +39,8 @@ public class BaseConstants {
     public static final String PARSE_ERROR = "解析失败";
 
     // TODO 需要改成Linux路径
-    public static final String XI_AN_SEISMIC_WORKSPACE_PATH = "E:/GIS小组专题图产出/专题图模板/地震专题图.smwu";
-    public static final String XI_AN_STORM_WORKSPACE_PATH = "E:/GIS小组专题图产出/专题图模板/暴雨专题图.smwu";
+    public static final String XI_AN_SEISMIC_WORKSPACE_PATH = "D:/制图/supermap/工作空间/地震专题图.smwu";
+    public static final String XI_AN_STORM_WORKSPACE_PATH = "D:/制图/supermap/工作空间/暴雨专题图.smwu";
     public static final String XI_AN_MAP_DATASETS_NAME = "西安智慧应急多灾害链平台";
     public static final String XI_AN_SEISMIC_DATASETS_NAME = "西安项目地震数据源";   // 放置地震数据源
     public static final String XI_AN_STORM_DATASETS_NAME = "西安项目暴雨数据源";     // 放置暴雨数据源
@@ -58,10 +58,10 @@ public class BaseConstants {
     public static final Integer THEMATIC_TYPE = 1;
     public static final Integer DOCUMENT_TYPE = 2;
     // TODO 需要改成Linux服务器路径
-    public static final String PICTURE_PREFIX = "D:/home/output/seismic-disaster/thematic/";
-    public static final String REPORTS_PREFIX = "D:/home/output/seismic-disaster/reports/";
-    public static final String STORM_PICTURE_PREFIX = "D:/home/output/storm-disaster/thematic/";
-    public static final String STORM_REPORTS_PREFIX = "D:/home/output/storm-disaster/reports/";
+    public static final String PICTURE_PREFIX = "D:/项目/home/output/seismic-disaster/thematic/";
+    public static final String REPORTS_PREFIX = "D:/项目/home/output/seismic-disaster/reports/";
+    public static final String STORM_PICTURE_PREFIX = "D:/项目/home/output/storm-disaster/thematic/";
+    public static final String STORM_REPORTS_PREFIX = "D:/项目/home/output/storm-disaster/reports/";
     public static final String HTTP_NGINX_PREFIX = "http://10.22.245.247:80";
 
     public static final String OUTPUT_FILED = "图件下载失败";
