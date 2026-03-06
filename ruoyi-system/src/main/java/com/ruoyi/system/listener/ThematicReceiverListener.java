ff53e23b9e994a1e3ffb39bd4dc38ef782c61ccd
zzw <2029503428@qq.com>
2026-03-06 20:39:03 +0800

    zzw部署版修改

--- ruoyi-system/src/main/java/com/ruoyi/system/listener/ThematicReceiverListener.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/listener/ThematicReceiverListener.java
@@ -20,6 +20,8 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.annotation.Resource;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -46,88 +48,102 @@ public class ThematicReceiverListener {
         // 打印日志
         log.info("rabbitmq 接收到 {} ...", outputDTO.getFileName());
 
+//        try {
+        // 设置图件产出信息
+        AssessmentOutput assessmentOutput = new AssessmentOutput();
+        assessmentOutput.setIsDeleted(0);    // 逻辑删除
+        assessmentOutput.setId(UUID.randomUUID().toString());    // 生成uuid
+        assessmentOutput.setCreateTime(LocalDateTime.now());     // 创建时间
+        assessmentOutput.setUpdateTime(LocalDateTime.now());     // 修改时间
+
+        File originFile = new File(outputDTO.getLocalSourceFile());
+
+        if (!originFile.exists()) {
+            throw new ThematicReceiveException(BaseConstants.FILE_NOT_FOUND_ERROR);
+        }
+
+        // 获取对应图片文件二进制流
+        // 获取对应图片文件二进制流
+        MultipartFile file;
         try {
-            // 设置图件产出信息
-            AssessmentOutput assessmentOutput = new AssessmentOutput();
-            assessmentOutput.setIsDeleted(0);    // 逻辑删除
-            assessmentOutput.setId(UUID.randomUUID().toString());    // 生成uuid
-            assessmentOutput.setCreateTime(LocalDateTime.now());     // 创建时间
-            assessmentOutput.setUpdateTime(LocalDateTime.now());     // 修改时间
+            file = new MockMultipartFile(originFile.getName(), originFile.getName(), "image/jpeg", new FileInputStream(originFile));
+        } catch (IOException e) {
+            log.error("文件未找到: {}", originFile.getAbsolutePath(), e);
+            throw new ThematicReceiveException(BaseConstants.FILE_NOT_FOUND_ERROR);
+        }
+        String imageUrl = outputDTO.getEqqueueId() + "_" + outputDTO.getFileName();
 
-            File originFile = new File(outputDTO.getLocalSourceFile());
-
-            if (!originFile.exists()) {
-                throw new ThematicReceiveException(BaseConstants.FILE_NOT_FOUND_ERROR);
-            }
-
-            // 获取对应图片文件二进制流
-            MultipartFile file = new MockMultipartFile(originFile.getName(), originFile.getName(), "image/jpeg", new FileInputStream(originFile));
-            // 将图片设置唯一Id
-            String imageUrl = outputDTO.getEqqueueId() + "_" + outputDTO.getFileName();
-
-            // 将图片上传到七牛云服务器
-            String qiniuUrl = qiniuOssUtil.upload(imageUrl, file);
-            // 上传失败
-            if (StringUtils.isEmpty(qiniuUrl)) {
-                throw new ThematicReceiveException(BaseConstants.THEMATIC_MAP_ERROR);
-            }
-            log.info("{} 成功上传到七牛云服务器...", outputDTO.getFileName());
-            // 数据拷贝
-            BeanUtils.copyProperties(outputDTO, assessmentOutput);
-            // 修改存储路径
-            assessmentOutput.setSourceFile(qiniuUrl);
-            // 将图件信息插入到结果表中
-            assessmentOutputMapper.insert(assessmentOutput);
-            log.info("{} 成功保存到数据库...", assessmentOutput.getFileName());
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            // 抛出异常
+        // 将图片上传到七牛云服务器
+        String qiniuUrl = qiniuOssUtil.upload(imageUrl, file);
+        // 上传失败
+        if (StringUtils.isEmpty(qiniuUrl)) {
             throw new ThematicReceiveException(BaseConstants.THEMATIC_MAP_ERROR);
         }
+        log.info("{} 成功上传到七牛云服务器...", outputDTO.getFileName());
+        // 数据拷贝
+        BeanUtils.copyProperties(outputDTO, assessmentOutput);
+        // 修改存储路径
+        assessmentOutput.setSourceFile(qiniuUrl);
+        // 将图件信息插入到结果表中
+        assessmentOutputMapper.insert(assessmentOutput);
+        log.info("{} 成功保存到数据库...", assessmentOutput.getFileName());
+
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//            // 抛出异常
+//            throw new ThematicReceiveException(BaseConstants.THEMATIC_MAP_ERROR);
+//        }
     }
     @RabbitListener(queues = "rain.map")
     public void receive(RainAssessmentOutputDTO outputDTO) {
         // 打印日志
         log.info("rabbitmq 接收到 {} ...", outputDTO.getFileName());
+//        try {
+        // 设置图件产出信息
+        RainAssessmentOutput assessmentOutput = new RainAssessmentOutput();
+        assessmentOutput.setIsDeleted(0);    // 逻辑删除
+        assessmentOutput.setId(UUID.randomUUID().toString());    // 生成uuid
+        assessmentOutput.setCreateTime(LocalDateTime.now());     // 创建时间
+        assessmentOutput.setUpdateTime(LocalDateTime.now());     // 修改时间
+
+        File originFile = new File(outputDTO.getLocalSourceFile());
+
+        if (!originFile.exists()) {
+            throw new ThematicReceiveException(BaseConstants.FILE_NOT_FOUND_ERROR);
+        }
+
+        // 获取对应图片文件二进制流
+
+        // 获取对应图片文件二进制流
+        MultipartFile file;
         try {
-            // 设置图件产出信息
-            RainAssessmentOutput assessmentOutput = new RainAssessmentOutput();
-            assessmentOutput.setIsDeleted(0);    // 逻辑删除
-            assessmentOutput.setId(UUID.randomUUID().toString());    // 生成uuid
-            assessmentOutput.setCreateTime(LocalDateTime.now());     // 创建时间
-            assessmentOutput.setUpdateTime(LocalDateTime.now());     // 修改时间
+            file = new MockMultipartFile(originFile.getName(), originFile.getName(), "image/jpeg", new FileInputStream(originFile));
+        } catch (IOException e) {
+            log.error("文件未找到: {}", originFile.getAbsolutePath(), e);
+            throw new ThematicReceiveException(BaseConstants.FILE_NOT_FOUND_ERROR);
+        }
+        // 将图片设置唯一Id
+        String imageUrl = outputDTO.getRainQueueId() + "_" + outputDTO.getFileName();
 
-            File originFile = new File(outputDTO.getLocalSourceFile());
-
-            if (!originFile.exists()) {
-                throw new ThematicReceiveException(BaseConstants.FILE_NOT_FOUND_ERROR);
-            }
-
-            // 获取对应图片文件二进制流
-            MultipartFile file = new MockMultipartFile(originFile.getName(), originFile.getName(), "image/jpeg", new FileInputStream(originFile));
-            // 将图片设置唯一Id
-            String imageUrl = outputDTO.getRainQueueId() + "_" + outputDTO.getFileName();
-
-            // 将图片上传到七牛云服务器
-            String qiniuUrl = qiniuOssUtil.upload(imageUrl, file);
-            // 上传失败
-            if (StringUtils.isEmpty(qiniuUrl)) {
-                throw new ThematicReceiveException(BaseConstants.THEMATIC_MAP_ERROR);
-            }
-            log.info("{} 成功上传到七牛云服务器...", outputDTO.getFileName());
-            // 数据拷贝
-            BeanUtils.copyProperties(outputDTO, assessmentOutput);
-            // 修改存储路径
-            assessmentOutput.setSourceFile(qiniuUrl);
-            // 将图件信息插入到结果表中
-            rainAssessmentOutputMapper.insert(assessmentOutput);
-            log.info("{} 成功保存到数据库...", assessmentOutput.getFileName());
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            // 抛出异常
+        // 将图片上传到七牛云服务器
+        String qiniuUrl = qiniuOssUtil.upload(imageUrl, file);
+        // 上传失败
+        if (StringUtils.isEmpty(qiniuUrl)) {
             throw new ThematicReceiveException(BaseConstants.THEMATIC_MAP_ERROR);
         }
+        log.info("{} 成功上传到七牛云服务器...", outputDTO.getFileName());
+        // 数据拷贝
+        BeanUtils.copyProperties(outputDTO, assessmentOutput);
+        // 修改存储路径
+        assessmentOutput.setSourceFile(qiniuUrl);
+        // 将图件信息插入到结果表中
+        rainAssessmentOutputMapper.insert(assessmentOutput);
+        log.info("{} 成功保存到数据库...", assessmentOutput.getFileName());
+
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//            // 抛出异常
+//            throw new ThematicReceiveException(BaseConstants.THEMATIC_MAP_ERROR);
+//        }
     }
 }
